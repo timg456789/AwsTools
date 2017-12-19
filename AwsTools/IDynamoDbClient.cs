d23865ea6219924e5c5888e3c084f62c340207e8
Timothy Gonzalez <timg456789@yahoo.com>
2017-12-19 00:52:59 -0500

    Expand interface.

--- AwsTools/IDynamoDbClient.cs
+++ AwsTools/IDynamoDbClient.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2.Model;
 
 namespace AwsTools
 {
     public interface IDynamoDbClient<T> where T : IModel, new()
     {
         Task<List<T>> Insert(List<T> ads);
+        Task<T> Get(T model);
+        Task<T> Get(Dictionary<string, AttributeValue> key);
     }
 }
